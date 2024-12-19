920ac7a29b58bd73f1f947fd35bb9fff12a61c71
elidsonX <ferreirapaeseliedson@gmail.com>
2024-12-18 21:08:24 -0300

    ajustes

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,7 @@
 function trocarPagina() {
     window.location.href = "../../pages/agenda/agenda.html"; // URL de destino
 }
+
+setTimeout(function() {
+    window.open("https://w.app/sXhMI6", "_blank"); // URL de destino
+  }, 2000);
