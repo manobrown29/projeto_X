45a5036cfed342936cdc7290a9eb8db8898b432f
elidsonX <ferreirapaeseliedson@gmail.com>
2024-12-20 17:12:11 -0300

    ajustes

--- pages/pix/pix.js
+++ pages/pix/pix.js
@@ -1,3 +1,13 @@
-function trocarPagina() {
-    window.location.href = "../../pages/sucesso/sucesso.html"; // URL de destino
+function copiarValor() {
+    // Recupera o valor do botão
+    const valor = document.getElementById("botaoCopiar").value;
+
+    // Usa a API Clipboard para copiar para a área de transferência
+    navigator.clipboard.writeText(valor)
+        .then(() => {
+            alert("Valor copiado"); // Exibe uma mensagem de sucesso
+        })
+        .catch(err => {
+            console.error("Erro ao copiar o valor: ", err); // Exibe o erro no console
+        });
 }
